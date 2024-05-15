ffbc920815f40924f9e2d31366471150996fe355
Luana Paiva <luanapaivace@gmail.com>
2024-05-15 14:01:24 -0300

    Estrutura de repetição while

--- estruturas-de-controle/Ex060.py
+++ estruturas-de-controle/Ex060.py
@@ -3,7 +3,7 @@ print("10 termos de uma PA")
 print("-=-" * 7)
 t = int(input("primeiro termo: "))
 r = int(input("razão: "))
-ter = 1
+ter = t
 cont = 1
 while cont <= 10:
     print("{} , ".format(ter), end="")
