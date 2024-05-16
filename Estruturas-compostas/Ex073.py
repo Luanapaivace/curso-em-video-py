ad759ff3c124a735b2dab69df21ddfd1e808cf23
Luana Paiva <luanapaivace@gmail.com>
2024-05-16 17:43:51 -0300

    Tuplas

--- Estruturas-compostas/Ex073.py
+++ Estruturas-compostas/Ex073.py
@@ -1,5 +1,5 @@
 from random import randint
-n1 = (randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100))
+n1 = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
 print("{}".format(n1))
 print("O maior número da lista é {}".format(max(n1)))
 print("O menor número da lista é {}".format(min(n1)))
